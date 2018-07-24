af81b99ebcb946c02d25505258ed2b6e8684248a
jane gong <janegongxw@gmail.com>
2018-07-24 15:55:08 +0800

    task w CRUD with Rails

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2018_07_24_034604) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
